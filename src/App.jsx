3c9381092c84cf28aaa052a5d2524544c5190b03
jvschamne <schamnejv@hotmail.com>
2023-04-02 09:36:30 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import MovieRow from './components/MovieRow'
+import Tmdb from './Tmdb'
+import FeaturedMovie from './components/FeaturedMovie'
+import './App.css'
+import Header from './components/Header'
+
+export default ( ) => {
+
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
+
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegar list total
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+
+      //Pegando o featured
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+    
+      
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo)
+      console.log(chosenInfo)
+    }
+    loadAll()
+  }, [])
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 100){
+        setBlackHeader(true)
+      }
+      else if(window.scrollY < 100){
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener)
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+
+  return(
+    <div className="page">
+
+      <Header black={blackHeader}/>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+      
+
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))
+
+        }
+
+      </section>
+
+
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+      </div>}
+
+    </div>
+  )
+}
